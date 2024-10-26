b17aff882eef8b187352365434f66cadc4c13564
Shahed Chowdhury Omi <shahedchowdhury120@gmail.com>
2024-10-26 13:48:25 +0600

    added identity signup in register

--- source code/Web/Controllers/UserController.cs
+++ source code/Web/Controllers/UserController.cs
@@ -1,14 +1,19 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Web.Helpers;
 using Web.Models.Business_Entities;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Web.Controllers
 {
     public class UserController : BaseController
     {
-
-        public UserController()
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public UserController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
         {
-
+            _signInManager = signInManager;
+            _userManager = userManager;
         }
 
         [HttpGet]
@@ -30,17 +35,40 @@ namespace Web.Controllers
         [HttpGet]
         public IActionResult Register()
         {
-            return View();
+            return View(new RegisterViewModel());
         }
 
         [HttpPost]
-        public IActionResult Register(RegisterViewModel model)
+        public async Task<IActionResult> Register(RegisterViewModel model)
         {
-            if (!ModelState.IsValid)
+            try
             {
-                return View(model);
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+
+                var user = new IdentityUser { UserName = model.Username, Email = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password!);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Tracking");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("RegistrationError", error.Description);
+                } 
             }
-            return RedirectToAction("Login");
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("RegistrationError", "An error occured during signing up");
+                HelperSerilog.LogError(ex.Message, ex);
+            }
+
+            return View(model);
         }
     }
 }
--- source code/Web/wwwroot/js/Tracking.js
+++ source code/Web/wwwroot/js/Tracking.js
@@ -1,96 +1,97 @@
-﻿
-$("#createModalButton").click(function (e) {
-    var viewNumber = 1;
-    $('#modalLabel').text("Create");
+﻿$(document).ready(function () {
+    $("#createModalButton").click(function (e) {
+        var viewNumber = 1;
+        $('#modalLabel').text("Create");
 
-    $.ajax({
-        type: "GET",
-        url: $(this).data('url') + '?viewNumber=' + viewNumber,
-        success: function (response) {
-            $('#loadModalData').html(response);
-        },
-        error: function () {
-            $('#loadModalData').html("Error Generating View");
-        }
-    })
+        $.ajax({
+            type: "GET",
+            url: $(this).data('url') + '?viewNumber=' + viewNumber,
+            success: function (response) {
+                $('#loadModalData').html(response);
+            },
+            error: function () {
+                $('#loadModalData').html("Error Generating View");
+            }
+        })
 
-    $('#exampleModal').modal("show");
+        $('#exampleModal').modal("show");
 
-});
+    });
 
-$('.Edit').click(function (e) {
-    e.preventDefault();
-    $('#modalLabel').html("Edit");
-    $('#loadModalData').html("Loading data while editing");
-
-    $.ajax({
-        type: "GET",
-        url: $(this).attr('href'),
-        success: function (response) {
-            $('#loadModalData').html(response);
-        },
-        error: function () {
-            $('#loadModalData').html("Error Generating View");
-        }
-    })
-
-    $('#exampleModal').modal("show");
-})
-
-$('#deleteAll').click(function(e) {
-    e.preventDefault();
-
-    $('#exampleModal #modalLabel').text("Do you want to delete all trackers?");
-
-    var deleteAllPermissionBtns = '<div class="p-2">'
-        + '<a class="btn btn-danger yes-delete-all" href="/Tracking/DeleteAll">Yes</a>'
-        + '<button class="btn btn-secondary mx-2 hideModal">No</button>'
-        + '</div>';
-
-    $('#exampleModal #loadModalData').html(deleteAllPermissionBtns);
-
-    $('#exampleModal').modal("show");
-
-});
-
-$('.delete-tracker').click(function (e) {
-    e.preventDefault();
-
-    var url = $(this).attr("href");
-
-    $('#exampleModal #modalLabel').text("Do you want to delete?");
-
-    var deletePermissionBtns = '<div class="p-2">'
-        + `<a class="btn btn-danger" href="${url}">Yes</a>`
-        + '<button class="btn btn-secondary mx-2 hideModal">No</button>'
-        + '</div>';
-
-    $('#exampleModal #loadModalData').html(deletePermissionBtns);
-    $('#exampleModal').modal("show");
-
-})
-
-// hide modal
-$('#exampleModal').on('click', '.hideModal', function (e) {
-    $("loadModalData").html("");
-    $('#exampleModal').modal("hide");
-});
-
-$(document).on("submit", "#registerForm", function (e) {
-    if ($("#Completed").val().length < 1 || $("#Planned").val().length < 1) {
-        alert("Both field are required");
+    $('.Edit').click(function (e) {
         e.preventDefault();
-    }
-});
+        $('#modalLabel').html("Edit");
+        $('#loadModalData').html("Loading data while editing");
 
-// add asterisk to all required fields
-$('input').each(function () {
-    var req = $(this).attr('data-val-required');
-    if (req != undefined) {
-        var label = $('label[for="' + $(this).attr('Id') + '"]');
-        $(label).addClass('required');
-    }
-})
+        $.ajax({
+            type: "GET",
+            url: $(this).attr('href'),
+            success: function (response) {
+                $('#loadModalData').html(response);
+            },
+            error: function () {
+                $('#loadModalData').html("Error Generating View");
+            }
+        })
+
+        $('#exampleModal').modal("show");
+    })
+
+    $('#deleteAll').click(function (e) {
+        e.preventDefault();
+
+        $('#exampleModal #modalLabel').text("Do you want to delete all trackers?");
+
+        var deleteAllPermissionBtns = '<div class="p-2">'
+            + '<a class="btn btn-danger yes-delete-all" href="/Tracking/DeleteAll">Yes</a>'
+            + '<button class="btn btn-secondary mx-2 hideModal">No</button>'
+            + '</div>';
+
+        $('#exampleModal #loadModalData').html(deleteAllPermissionBtns);
+
+        $('#exampleModal').modal("show");
+
+    });
+
+    $('.delete-tracker').click(function (e) {
+        e.preventDefault();
+
+        var url = $(this).attr("href");
+
+        $('#exampleModal #modalLabel').text("Do you want to delete?");
+
+        var deletePermissionBtns = '<div class="p-2">'
+            + `<a class="btn btn-danger" href="${url}">Yes</a>`
+            + '<button class="btn btn-secondary mx-2 hideModal">No</button>'
+            + '</div>';
+
+        $('#exampleModal #loadModalData').html(deletePermissionBtns);
+        $('#exampleModal').modal("show");
+
+    })
+
+    // hide modal
+    $('#exampleModal').on('click', '.hideModal', function (e) {
+        $("loadModalData").html("");
+        $('#exampleModal').modal("hide");
+    });
+
+    $(document).on("submit", "#registerForm", function (e) {
+        if ($("#Completed").val().length < 1 || $("#Planned").val().length < 1) {
+            alert("Both field are required");
+            e.preventDefault();
+        }
+    });
+
+    // add asterisk to all required fields
+    $('input').each(function () {
+        var req = $(this).attr('data-val-required');
+        if (req != undefined) {
+            var label = $('label[for="' + $(this).attr('Id') + '"]');
+            $(label).addClass('required');
+        }
+    })
+});
 
 // password view
 function PasswordToggle(fieldId, passwordIconId) {
