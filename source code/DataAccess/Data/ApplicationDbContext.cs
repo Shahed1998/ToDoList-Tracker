7d2c3e503256c8a28531b8968065ed7de41f55ed
Shahed Chowdhury Omi <shahedchowdhury120@gmail.com>
2025-03-27 14:56:46 +0600

    update - 2.0 setup

---
+++ source code/DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
