694d11bf24723470b76e9e590dcabe930ab0c1c8
Shahed Chowdhury Omi <shahedchowdhury120@gmail.com>
2025-06-08 11:24:45 +0600

    isolated partial view completely

--- source code/ToDoListTracker/Models/Business Entities/IndexVM.cs
+++ source code/ToDoListTracker/Models/Business Entities/IndexVM.cs
@@ -5,7 +5,7 @@
         public bool IsSaved { get; set; } = false;
         public bool Notify { get; set; } = false;
         public int pageNumber { get; set; } = 1;
-        public int pageSize { get; set; } = 30;
+        public int pageSize { get; set; } = 5;
         public int count { get; set; } = 0;
         public bool prevPage { get; set; } = false;
         public string message { get; set; } = "";
